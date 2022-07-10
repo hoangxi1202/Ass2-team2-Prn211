67f1cf336513805f25aa9e994ed653bbb57695ed
trieudo1101 <trieudo1103@gmail.com>
2022-07-10 20:49:00 +0700

    fix-product

--- DataAccess/Repository/IProductRepository.cs
+++ DataAccess/Repository/IProductRepository.cs
@@ -13,9 +13,6 @@ namespace DataAccess.Repository
         public void CreateProduct(Product product);
         public void UpdateProduct(Product product);
         public void DeleteProduct(int productID);
-        public Product GetProductById(int productID);
-        public List<Product> GetProductsByName(string name);
-        public List<Product> GetProductByUnitPrice(decimal p1, decimal p2);
-        public List<Product> GetProductByUnitInStock(int p1, int p2);
+        
     }
 }
